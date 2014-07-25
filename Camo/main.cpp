c8dd7e73c5798fb76955970c83cd919f2efff606
Yonsm <Yonsm@msn.com>
2014-07-25 17:40:50 +0800

    test

--- Camo/main.cpp
+++ Camo/main.cpp
@@ -298,7 +298,7 @@ private:
 int main(int argc, char * argv[])
 {
 	SourceParser parser;
-	//parser.ParseFile("/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS7.1.sdk/Developer/Library/Frameworks/SenTestingKit.framework/Headers/SenTestLog.h");
+	//parser.ParseFile("/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS7.1.sdk/System/Library/Frameworks/Foundation.framework/Headers/NSRunLoop.h");
 	parser.ParseDir("/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer");
 	//parser.ParseDir("/Users/Yonsm/Documents/GitHub/Sample");
 	return 0;
